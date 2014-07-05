23eeceb01160ebbfe0a9e3cc1bbe3d74c8081fe9
andres <andres@andres.com>
2014-07-04 22:42:32 -0400

    Update README

--- CustomListItems/src/com/ocd/dev/customlistitems/BadPeopleAdapter.java
+++ CustomListItems/src/com/ocd/dev/customlistitems/BadPeopleAdapter.java
@@ -11,49 +11,49 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BadPeopleAdapter extends ArrayAdapter<Person> {
-		private LayoutInflater mInflater;
-		private List<Person> mPeople;
+	private LayoutInflater mInflater;
+	private List<Person> mPeople;
 
-		public BadPeopleAdapter(Context context, int resource,
-				List<Person> objects) {
-			super(context, resource, objects);
-			mInflater = LayoutInflater.from(context);
-			mPeople = objects;
-		}
-		
-		public BadPeopleAdapter(Context context, List<Person> objects) {
-			// what on earth is this second argument to super??
-			super(context, 0, objects);
-			mInflater = LayoutInflater.from(context);
-			mPeople = objects;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View view;
-			ViewHolder holder;
-			if(convertView == null) {
-				view = mInflater.inflate(R.layout.row_layout, parent, false);
-				holder = new ViewHolder();
-				holder.avatar = (ImageView)view.findViewById(R.id.avatar);
-				holder.name = (TextView)view.findViewById(R.id.name);
-				holder.latestMessage = (TextView)view.findViewById(R.id.latest_message);
-				view.setTag(holder);
-			} else {
-				view = convertView;
-				holder = (ViewHolder)view.getTag();
-			}
-			
-			Person person = mPeople.get(position);
-			holder.avatar.setImageBitmap(person.getAvatar());
-			holder.name.setText(person.getName());
-			holder.latestMessage.setText(person.getLatestMessage());
-			
-			return view;
-		}
-		
-		private class ViewHolder {
-			public ImageView avatar;
-			public TextView name, latestMessage;
-		}
+	public BadPeopleAdapter(Context context, int resource,
+			List<Person> objects) {
+		super(context, resource, objects);
+		mInflater = LayoutInflater.from(context);
+		mPeople = objects;
 	}
+
+	public BadPeopleAdapter(Context context, List<Person> objects) {
+		// what on earth is this second argument to super??
+		super(context, 0, objects);
+		mInflater = LayoutInflater.from(context);
+		mPeople = objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view;
+		ViewHolder holder;
+		if(convertView == null) {
+			view = mInflater.inflate(R.layout.row_layout, parent, false);
+			holder = new ViewHolder();
+			holder.avatar = (ImageView)view.findViewById(R.id.avatar);
+			holder.name = (TextView)view.findViewById(R.id.name);
+			holder.latestMessage = (TextView)view.findViewById(R.id.latest_message);
+			view.setTag(holder);
+		} else {
+			view = convertView;
+			holder = (ViewHolder)view.getTag();
+		}
+
+		Person person = mPeople.get(position);
+		holder.avatar.setImageBitmap(person.getAvatar());
+		holder.name.setText(person.getName());
+		holder.latestMessage.setText(person.getLatestMessage());
+
+		return view;
+	}
+
+	private class ViewHolder {
+		public ImageView avatar;
+		public TextView name, latestMessage;
+	}
+}
--- README.md
+++ README.md
@@ -6,9 +6,11 @@ If the project you are running uses Google Play Services, you will have to impor
 
 1. [Using a translucent action bar with Google Maps](http://www.ocddevelopers.com/2014/using-a-translucent-action-bar-with-google-maps/)
 
-2. [Displaying a Grocery List with the GDK](http://www.ocddevelopers.com/2014/displaying-a-grocery-list-with-the-gdk/)
+2. [Displaying a grocery List with the GDK](http://www.ocddevelopers.com/2014/displaying-a-grocery-list-with-the-gdk/)
 
 3. [Menus and voice input with the GDK](http://www.ocddevelopers.com/2014/menus-and-voice-input-with-the-gdk/)
+
+4. [Extend BaseAdapter instead of ArrayAdapter for custom list items](http://www.ocddevelopers.com/2014/extend-baseadapter-instead-of-arrayadapter-for-custom-list-items/)
 
 
 Happy coding!
